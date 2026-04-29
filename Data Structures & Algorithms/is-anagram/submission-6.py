eb7511bf04db1648ae54aefdff615fbe623951a3
Hammad Habib <180171855+Hammad64-bits@users.noreply.github.com>
2026-04-29 11:14:16 +0500

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+
+        lookup = dict()
+        for char in s:
+            
+            if lookup.get(char) == None :
+                lookup[char] = 1
+            else: 
+                lookup[char] += 1
+        print(lookup)
+        for char in t:
+
+            if char in lookup:
+                if lookup[char] == 0: return False
+                lookup[char] -= 1
+            else:
+                return False
+        print(lookup)
+        return True
+
+        
